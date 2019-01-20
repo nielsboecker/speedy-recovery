b0c279f39ae73fa5aec19d930d931542eb55a297
Joshua Zeltser <joshua.zeltser@gmail.com>
2019-01-20 18:31:32 +0000

    I have now been able to create a basic calendar which can write in events saved in a file and also have events added to it manually (although they are not saved). More work still needs to be done on the various functionalities that are provided.

--- speedy-recovery-client/src/App.js
+++ speedy-recovery-client/src/App.js
@@ -1,14 +1,61 @@
-import React, { Component } from 'react';
-import './App.css';
+import React from 'react';
+import events from '../src/events'
 
-class App extends Component {
+import BigCalendar from 'react-big-calendar';
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+const localizer = BigCalendar.momentLocalizer(moment);
+
+moment.locale('en-GB');
+
+const propTypes = {};
+
+class App extends React.Component {
+  constructor(...args) {
+    super(...args);
+
+    this.state = { events }
+  }
+
+  handleSelect = ({start, end}) => {
+    const title = window.prompt('New Event name');
+    if (title)
+      this.setState({
+        events: [
+          ...this.state.events,
+          {
+            start,
+            end,
+            title,
+
+          },
+        ],
+      })
+  };
+
+
   render() {
     return (
-      <div className="App">
-          <p>Hello, World. Speedy Recovery! :-)</p>
-      </div>
+        <div style={{height: 550}}>
+          <BigCalendar
+              selectable
+              localizer={localizer}
+              events={this.state.events}
+              step={60}
+              view='week'
+              views={['week']}
+              min={new Date(2008, 0, 1, 8, 0)} // 8.00 AM
+              max={new Date(2008, 0, 1, 17, 0)} // Max will be 6.00 PM!
+              date={new Date(2018, 0, 1)}
+              onSelectEvent={event => alert(event.title)}
+              onSelectSlot={this.handleSelect}
+          />
+        </div>
     );
   }
 }
+
+App.propTypes = propTypes;
 
 export default App;
--- speedy-recovery-client/src/index.js
+++ speedy-recovery-client/src/index.js
@@ -1,32 +1,7 @@
 import React from 'react';
+import './index.css';
+import App from './App';
 import { render } from 'react-dom';
-import BigCalendar from 'react-big-calendar';
-import moment from 'moment';
-import 'react-big-calendar/lib/css/react-big-calendar.css';
 
-moment.locale('en-GB');
-const localizer = BigCalendar.momentLocalizer(moment);
-
-const App = () => (
-    <div style={{ height: 700 }}>
-        <BigCalendar
-            localizer={localizer}
-            events={[
-                {
-                    'title': 'My event',
-                    'allDay': false,
-                    'start': new Date(2018, 0, 1, 10, 0), // 10.00 AM
-                    'end': new Date(2018, 0, 1, 14, 0), // 2.00 PM
-                }
-            ]}
-            step={60}
-            view='week'
-            views={['week']}
-            min={new Date(2008, 0, 1, 8, 0)} // 8.00 AM
-            max={new Date(2008, 0, 1, 17, 0)} // Max will be 6.00 PM!
-            date={new Date(2018, 0, 1)}
-        />
-    </div>
-);
 
 render(<App />, document.getElementById('root'));
