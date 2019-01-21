533a107327002aa877837a1a0236c04dac78c752
Joshua Zeltser <joshua.zeltser@gmail.com>
2019-01-21 18:14:47 +0000

    I have added functionality so that multiple views of the calendar can be displayed. I have also fixed the next/prev toggle buttons.

--- speedy-recovery-client/src/App.js
+++ speedy-recovery-client/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import events from '../src/events'
-
 import BigCalendar from 'react-big-calendar';
 import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
@@ -10,6 +9,8 @@ const localizer = BigCalendar.momentLocalizer(moment);
 moment.locale('en-GB');
 
 const propTypes = {};
+
+let allViews = Object.keys(BigCalendar.Views).map(k => BigCalendar.Views[k]);
 
 class App extends React.Component {
   constructor(...args) {
@@ -43,11 +44,11 @@ class App extends React.Component {
               localizer={localizer}
               events={this.state.events}
               step={60}
-              view='week'
-              views={['week']}
-              min={new Date(2008, 0, 1, 8, 0)} // 8.00 AM
-              max={new Date(2008, 0, 1, 17, 0)} // Max will be 6.00 PM!
-              date={new Date(2018, 0, 1)}
+              views={allViews}
+              // min={new Date(2008, 0, 1, 8, 0)} // 8.00 AM
+              // max={new Date(2008, 0, 1, 17, 0)} // Max will be 6.00 PM!
+              // date={new Date(2018, 0, 1)}
+              defaultDate={new Date()}
               onSelectEvent={event => alert(event.title)}
               onSelectSlot={this.handleSelect}
           />
