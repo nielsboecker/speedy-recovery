4719094eefe99f959bea057c6609df790b155b1d
Niels Boecker <nielsboecker@gmail.com>
2019-01-22 22:41:43 +0000

    Add react-router-dom dependency and create basic routing

--- speedy-recovery-client/src/components/core/App.js
+++ speedy-recovery-client/src/components/core/App.js
@@ -1,14 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 import Header from '../shared/Header';
 import StartPage from '../start/StartPage';
+import HomePage from '../home/HomePage';
+import CalendarPage from '../calendar/CalendarPage';
+import MessagingPage from '../messaging/MessagingPage';
+import ProfilePage from '../profile/ProfilePage';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header/>
-         <StartPage/>
+        <Router>
+          <Fragment>
+            <Header/>
+
+            <Route exact path="/" component={StartPage} />
+            <Route path="/home" component={HomePage} />
+            <Route path="/calendar" component={CalendarPage} />
+            <Route path="/messaging" component={MessagingPage} />
+            <Route path="/profile" component={ProfilePage} />
+          </Fragment>
+        </Router>
       </div>
     );
   }
