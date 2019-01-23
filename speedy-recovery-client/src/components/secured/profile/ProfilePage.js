952b36804ad41f0c08677511cfe9de8426470890
Niels Boecker <nielsboecker@gmail.com>
2019-01-23 01:06:33 +0000

    Improve Header

--- speedy-recovery-client/src/components/secured/profile/ProfilePage.js
+++ speedy-recovery-client/src/components/secured/profile/ProfilePage.js
@@ -1,10 +1,27 @@
 import React, {Component} from 'react';
+import { Divider, Icon, Table } from 'semantic-ui-react'
 
 class ProfilePage extends Component {
     render() {
         return (
             <div>
                 <h1>Profile</h1>
+
+                <Table definition>
+                    <Table.Body>
+                        <Table.Row>
+                            <Table.Cell width={2}>Name</Table.Cell>
+                            <Table.Cell>Robert Smith</Table.Cell>
+                        </Table.Row>
+                        <Table.Row>
+                            <Table.Cell>Role</Table.Cell>
+                            <Table.Cell>
+                                <Icon name="user doctor"/>
+                                 Doctor
+                            </Table.Cell>
+                        </Table.Row>
+                    </Table.Body>
+                </Table>
             </div>
         );
     }
