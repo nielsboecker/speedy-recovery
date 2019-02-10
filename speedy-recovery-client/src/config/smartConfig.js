597888326a86d33ff49e2f4894c81e921e5c94d1
Niels Boecker <nielsboecker@gmail.com>
2019-02-10 20:06:02 +0000

    Add a provider sandbox config

--- speedy-recovery-client/src/config/smartConfig.js
+++ speedy-recovery-client/src/config/smartConfig.js
@@ -1,6 +1,9 @@
 const smartSandboxConfigs = {
-  // stand-alone patient launch for Shelby
-  patientSandbox_1: "https://launch.smarthealthit.org/v/r3/sim/eyJrIjoiMSIsImIiOiJkMGQwY2RlMC00YjIxLTQyZjYtOWMxZS1iZmE0NDdkNzIwNTkifQ/fhir"
+  // Patient launch (Shelby Von, ID = d0d0cde0-4b21-42f6-9c1e-bfa447d72059)
+  patientSandbox_1: "https://launch.smarthealthit.org/v/r3/sim/eyJrIjoiMSIsImIiOiJkMGQwY2RlMC00YjIxLTQyZjYtOWMxZS1iZmE0NDdkNzIwNTkifQ/fhir",
+
+  // Provider launch (provider is Joseph P Nichols, sees three patients including Shelby Von)
+  providerSandbox_1: "https://launch.smarthealthit.org/v/r3/sim/eyJoIjoiMSIsImIiOiJkMGQwY2RlMC00YjIxLTQyZjYtOWMxZS1iZmE0NDdkNzIwNTksc21hcnQtNzc3NzcwMSwyMmE5YmMyYi0wYTE1LTRkZGItODUxOC0xYTZmMGQ4MWEyODYiLCJlIjoic21hcnQtUHJhY3RpdGlvbmVyLTcyMDgwNDE2In0/fhir"
 };
 
 const smartConfig  = {
