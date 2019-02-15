2c241011eebe90d3e346df500b6c0f519d1bf4b5
fabihaahmed <fabiha.ahmed@icloud.com>
2019-02-15 05:59:11 +0000

    Added test for InformationPage

---
+++ speedy-recovery-client/src/__tests__/InfoPage.test.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Enzyme, { shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import exampleUser from "./test_input/internal/ExampleUser.json";
+import InfoFactory from "../components/secured/patientInformation/InfoFactory";
+import PatientInfo from "../components/secured/patientInformation/PatientInfo";
+import ParentInfo from "../components/secured/patientInformation/ParentInfo";
+import PractitionerInfo from "../components/secured/patientInformation/PractitionerInfo";
+import fhirExamplePatient from "./test_input/fhir_r3/FhirExamplePatient.json";
+
+Enzyme.configure({ adapter: new Adapter() });
+
+it("renders without crashing", () => {
+  shallow(<InfoFactory user= {exampleUser}  patResource={fhirExamplePatient}/>);
+});
+
+it("renders without crashing", () => {
+    shallow(<PractitionerInfo user= {exampleUser} extraInfo={fhirExamplePatient}/>);
+});
+
+it("renders without crashing", () => {
+    shallow(<PatientInfo user= {exampleUser}/>);
+});
+
+it("renders without crashing", () => {
+    shallow(<ParentInfo user= {exampleUser}/>);
+});
+
+
