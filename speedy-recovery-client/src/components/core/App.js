bf6df7bdb5136e488e79a75f78ec8ad4a8a8c961
Niels Boecker <nielsboecker@gmail.com>
2019-02-18 17:57:06 +0000

    Add FHIR server capability check and display error message if not satisfied

--- speedy-recovery-client/src/components/core/App.js
+++ speedy-recovery-client/src/components/core/App.js
@@ -5,6 +5,7 @@ import ErrorPage from "../error/ErrorPage";
 import LandingMainPage from "../landing/core/LandingMainPage";
 import SecuredMainPage from "../secured/core/SecuredMainPage";
 import SmartAuthService from "../../service/SmartAuthService";
+import FhirServerService from "../../service/FhirServerService";
 import { mapPatientToUser } from "../../dataaccess/FhirDataAdapter";
 
 class App extends Component {
@@ -59,6 +60,12 @@ class App extends Component {
   };
 
   componentWillMount = () => {
+    // Check FHIR server capability
+    FhirServerService.checkFhirCapabilityStatement()
+      .then(result => console.log("FHIR capability check successful", result))
+      .catch(() => this.handleFhirServerError());
+
+    // Register SMART auth callback
     SmartAuthService.onSmartAuthenticatedSessionReady()
       .then(fhirClient => this.handleLoginSuccess(fhirClient))
       .catch(errorMessage => this.handleLoginError(errorMessage));
@@ -119,6 +126,16 @@ class App extends Component {
         rootCause: "SMART_AUTH",
         message: errorMessage,
         resolvable: true
+      }
+    });
+  };
+
+  handleFhirServerError = () => {
+    this.setState({
+      error: {
+        rootCause: "FHIR_SERVER",
+        message: "The EHR's FHIR server does not provide the required functionality.",
+        resolvable: false
       }
     });
   };
