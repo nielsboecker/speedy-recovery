aa806ca87dc29b3d96a89f17176856d325c730c8
Joshua Zeltser <joshua.zeltser@gmail.com>
2019-02-18 15:59:17 +0000

    Created mapping functions for condition and medication FHIR resources with testing

--- speedy-recovery-client/src/__tests__/FhirDataAdapter.test.js
+++ speedy-recovery-client/src/__tests__/FhirDataAdapter.test.js
@@ -1,9 +1,13 @@
 import {
   mapAppointment,
+  mapCondition,
+  mapMedication,
   mapPatientToUser
 } from "../dataaccess/FhirDataAdapter";
 import fhirExamplePatient from "./test_input/fhir_r3/FhirExamplePatient.json";
 import fhirShortApp from "./test_input/fhir_r3/FhirShortExampleAppointments.json";
+import fhirExampleCondition from "./test_input/fhir_r3/FhirExampleCondition.json";
+import fhirExampleMedication from "./test_input/fhir_r3/FhirExampleMedication.json";
 
 test("map FHIR Patient resource to internal user data model", () => {
   const user = mapPatientToUser(fhirExamplePatient);
@@ -13,4 +17,14 @@ test("map FHIR Patient resource to internal user data model", () => {
 test("map FHIR Appointment resource to calendar data model", () => {
   const appointment = mapAppointment(fhirShortApp);
   expect(appointment).toMatchSnapshot();
+});
+
+test("map FHIR Condition resource to internal data model", () => {
+  const condition = mapCondition(fhirExampleCondition);
+  expect(condition).toMatchSnapshot();
+});
+
+test("map FHIR Medication resource to internal data model", () => {
+  const medication = mapMedication(fhirExampleMedication);
+  expect(medication).toMatchSnapshot();
 });
