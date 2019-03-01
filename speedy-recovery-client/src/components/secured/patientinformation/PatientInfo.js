96546b708239db59e3711beef9ea2485aac9df61
Ying <psyyh2@exmail.nottingham.ac.uk>
2019-03-01 20:59:39 +0000

    Split patient info page to subsections (basic, condition, medication, careplan...)

--- speedy-recovery-client/src/components/secured/patientinformation/PatientInfo.js
+++ speedy-recovery-client/src/components/secured/patientinformation/PatientInfo.js
@@ -1,18 +1,30 @@
 import React, { Component } from "react";
+import { Tab } from 'semantic-ui-react'
+
 
 class PatientInfo extends Component {
+
   render() {
     const { user } = this.props;
+    const panes = [
+          { menuItem: 'Basic', render: () => <Tab.Pane><h4>Name: {user.name}</h4><h4>Gender: {user.gender}</h4><h4>Birthday: {user.birthDate}</h4></Tab.Pane> },
+          { menuItem: 'Medication', render: () => <Tab.Pane>Medication Content</Tab.Pane> },
+          { menuItem: 'Condition', render: () => <Tab.Pane>Condition Content</Tab.Pane> },
+          { menuItem: 'CarePlan', render: () => <Tab.Pane>Care Plan Content</Tab.Pane> }
+    ]
 
-    return (
+
+      return (
+
       <div>
-        <h1>Patient Information For Patient View</h1>
-        <h4>Name: {user.name}</h4>
-        <h4>Gender: {user.gender}</h4>
-        <h4>Birthday: {user.birthDate}</h4>
+          <h1>Patient Information For Patient View</h1>
+          <Tab menu={{ fluid: true, vertical: true, tabular: true }} panes={panes} />
       </div>
-    );
+
+
+  );
   }
 }
+
 
 export default PatientInfo;
