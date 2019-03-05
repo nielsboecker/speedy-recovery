e110cb65509c78a3e0729f12faab5c63462cf38e
fabihaahmed <fabiha.ahmed@icloud.com>
2019-03-05 16:43:46 +0000

    Started trying an axios mock framework to test backendService

--- speedy-recovery-client/src/__tests__/service/BackendService.test.js
+++ speedy-recovery-client/src/__tests__/service/BackendService.test.js
@@ -1,54 +1,43 @@
+import mockAxios from 'jest-mock-axios';
 import {
     getConversation,
     getMessages,
+    postMessages,
     getPractitionerInfo
 } from "../../service/BackendService";
 
- test('Test to fetch Conversations from backend', async() => {    
-    const get = jest.fn().mockImplementation((id)=> 
-    Promise.resolve({
-        status : 200,
-        data : [{
-            Conversation_Id: 5,
-            alt: "alt",
-            date: "2019-03-03T21:15:38.000Z",
-            subtitle: "ok",
-            unread: 2,
-            userid1: "smart-Practitioner-72080416",
-            userid2: "user8"
-        }]
-    })
-    );  
-    const response = await get('smart-Practitioner-72080416');
-    console.log("the response is", response);
-    expect(response.status).toBe(200);  
-    expect(response.data[0].Conversation_Id).toBe(5);
-    expect(get).toHaveBeenCalledTimes(1);
-    expect(get).toHaveBeenCalledWith('smart-Practitioner-72080416');
+test("testing dummy", () =>{
+  expect(2+2).toBe(4);
 });
 
-test('Test to fetch Messages from backend', async() => {  
-    //set up  
-    const get = jest.fn().mockImplementation((id1, id2)=> 
-    Promise.resolve({
-        status : 200,
-        data : [{
-            Conversation_Id: 9,
-            ID: 20,
-            Message: "Hi eva",
-            Recipient: "user8",
-            Sender: "smart-Practitioner-72080416",
-            time: "2019-03-01T15:01:46.000Z"
-        }]
-    })
-    );
-    //work 
-    const response = await get('smart-Practitioner-72080416','user8');
-    //console.log("the response is", response);
-    //expect
-    expect(response.status).toBe(200);  
-    expect(response.data[0].Conversation_Id).toBe(9);
-    expect(get).toHaveBeenCalledTimes(1);
-    expect(get).toHaveBeenCalledWith('smart-Practitioner-72080416', 'user8');
-});
+// TODO 
+// afterEach(() => {
+//     // cleaning up the mess left behind the previous test
+//     mockAxios.reset();
+// });
+
+
+//  test('Test to Post messages to backend', () =>{
+//     let thenFunction = jest.fn();
+//     let catchFunction = jest.fn();
+//     let senderID = "smart-Practitioner-72080416";
+//     let RecipientID = "d0d0cde0-4b21-42f6-9c1e-bfa447d";
+//     let message = "Hi i am testing hereeeeeeeeee";
+//     postMessages(senderID,RecipientID,message)
+//     .then(thenFunction)
+//     .catch(catchFunction);
+     
+    
+ 
+//     expect(mockAxios.post).toHaveBeenCalledWith("https://speedy-recovery-server.azurewebsites.net/messages?",
+//      {data: {
+//          senderID, 
+//          RecipientID,
+//          message
+//       }});
+
+//       let responseObj = 'server says hello!' ;
+//     mockAxios.mockResponse(responseObj);
+       
+//  });
 
