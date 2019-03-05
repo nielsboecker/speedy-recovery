9b7f19b979e9ee6e984aaeba15659f36e42c1134
ansonwong9695 <603110911@qq.com>
2019-03-05 14:46:26 +0000

    change the condition

--- speedy-recovery-client/src/components/secured/home/HomePageForPractitioner.js
+++ speedy-recovery-client/src/components/secured/home/HomePageForPractitioner.js
@@ -3,58 +3,36 @@ import { Link } from "react-router-dom";
 import { Loader } from "semantic-ui-react";
 
 class HomePageForPractitioner extends Component {
+  render(){
+    if (this.props.user && this.props.user.name&&this.props.event.start&&this.props.event.patient ){
+      if(this.props.event.start && this.props.event.patient){
+        return (
+          <div>
+            <p>Hello {this.props.user.name}</p>
+            <p>
+              Here is the time for your patient {this.props.event.patient}'s next
+              appointment:{" "}
+              <Link to={"/secured/calendar"}>
+                {new Date(this.props.event.start).toLocaleString("en-uk")}
+              </Link>
+            </p>
+          </div>
+          )}else{
+          return(
+          <div>
+            <p>Hello {this.props.user.name}</p>
+            <p>You dont't have any appointment</p>
+          </div> )
+        }
 
-  constructor(props) {
-    super(props);
-    this.state = { loading: true };
-    this.bool=true;
-  }
-
-  static defaultProps = {
-    event: {
-      start: "no appointment time",
-
+      }else{
+      return <Loader content='Loading' active inline='centered' size='large'/>;
     }
-  };
-
-  componentDidUpdate(prevProps) {
-    const cProps=JSON.stringify(this.props);
-    const pProps=JSON.stringify(prevProps);
-    if(cProps!==pProps)
-    {
-      this.bool=false;
-    }else{
-      this.props.event.start="Invalid Date"
-      this.bool=false;
     }
-  }
 
-  updateHomePage(){
-     if(this.bool){
-       return <Loader content='Loading' active inline='centered' size='large'/>;
-     }else{
-       return ( <div>
-         <p>Hello {this.props.user.name}</p>
-         {new Date(this.props.event.start).toLocaleString("en-uk") !==
-         "Invalid Date" ? (
-           <p>
-             Here is the time for your patient {this.props.event.patient}'s next
-             appointment:{" "}
-             <Link to={"/secured/calendar"}>
-               {new Date(this.props.event.start).toLocaleString("en-uk")}
-             </Link>
-           </p>
-         ) : (
-           <p>You dont't have any appointment</p>
-         )}
-       </div>)
-     }
-  }
-
-
-  render() {
-    return this.updateHomePage();
-  }
 }
+
+
+
 
 export default HomePageForPractitioner;
