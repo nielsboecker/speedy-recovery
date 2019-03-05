7ae6aede1e7e584b7aa4d9d1b111dfb81d6e3840
Ying <psyyh2@exmail.nottingham.ac.uk>
2019-03-05 13:25:19 +0000

    Modify kid Basic

--- speedy-recovery-client/src/components/secured/patientinformation/PatientInfo.js
+++ speedy-recovery-client/src/components/secured/patientinformation/PatientInfo.js
@@ -143,6 +143,30 @@ class PatientInfo extends Component {
       return table;
     };
 
+    const showGenderText = () => {
+      let genderStr = "";
+      let gender = user.gender;
+      if (gender === "female") {
+        genderStr = "Girl";
+      } else if (gender === "male") {
+        genderStr = "Boy";
+      } else {
+        genderStr = gender;
+      }
+      return genderStr;
+    };
+
+    const showGenderIcon = () => {
+      let genderIcon = <Icon fitted name="smile outline" />;
+      let gender = user.gender;
+      if (gender === "female") {
+        genderIcon = <Icon color="pink" fitted name="woman" />;
+      } else if (gender === "male") {
+        genderIcon = <Icon color="blue" fitted name="man" />;
+      }
+      return genderIcon;
+    };
+
     const panes = [
       {
         menuItem: (
@@ -151,11 +175,17 @@ class PatientInfo extends Component {
             Basic
           </Menu.Item>
         ),
+
         render: () => (
           <Tab.Pane>
             <h4>Name: {user.name}</h4>
-            <h4>Gender: {user.gender}</h4>
-            <h4>Birthday: {user.birthDate}</h4>
+            <h4>
+              Gender: {showGenderText()}
+              {showGenderIcon()}
+            </h4>
+            <h4>
+              Birthday: {user.birthDate} <Icon fitted name="birthday" />
+            </h4>
           </Tab.Pane>
         )
       },
