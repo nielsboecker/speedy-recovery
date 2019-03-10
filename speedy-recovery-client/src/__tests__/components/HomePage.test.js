9a1edea216c86563e10313b63bb62058dd96bad7
Niels Boecker <nielsboecker@gmail.com>
2019-03-10 18:46:32 +0000

    Remove unnecessary props in HomePage.test.js

--- speedy-recovery-client/src/__tests__/components/HomePage.test.js
+++ speedy-recovery-client/src/__tests__/components/HomePage.test.js
@@ -1,41 +1,26 @@
 import React from "react";
 import Enzyme, { shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
-import exampleUser from "../test_input/internal/ExampleUser.json";
-import fhirExamplePatient from "../test_input/fhir_resources_stu3/FhirExamplePatientSTU3.json";
-import exampleAppointment from "../test_input/internal/ExampleAppointment";
-import HomePageForPatient from "../../components/secured/home/HomePageForPatient";
-import HomePageForParent from "../../components/secured/home/HomePageForParent";
-import HomePageForPractitioner from "../../components/secured/home/HomePageForPractitioner";
+import PatientHomePage from "../../components/secured/home/PatientHomePage";
+import ParentHomePage from "../../components/secured/home/ParentHomePage";
+import PractitionerHomePage from "../../components/secured/home/PractitionerHomePage";
 
 Enzyme.configure({ adapter: new Adapter() });
 
 it("Patient's home page renders without crashing", () => {
   shallow(
-    <HomePageForPatient
-      user={exampleUser}
-      patient={fhirExamplePatient}
-      event={exampleAppointment}
-    />
+    <PatientHomePage/>
   );
 });
 
 it("Parent's home page renders without crashing", () => {
   shallow(
-    <HomePageForParent
-      user={exampleUser}
-      patient={fhirExamplePatient}
-      event={exampleAppointment}
-    />
+    <ParentHomePage/>
   );
 });
 
 it("Practitioner's home page renders without crashing", () => {
   shallow(
-    <HomePageForPractitioner
-      user={exampleUser}
-      patient={fhirExamplePatient}
-      event={exampleAppointment}
-    />
+    <PractitionerHomePage/>
   );
 });
