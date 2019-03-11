65575ed871482a88663209912448d67295eccfe4
Ying <psyyh2@exmail.nottingham.ac.uk>
2019-03-11 02:29:24 +0000

    extract medicationDispense pane

--- speedy-recovery-client/src/components/secured/patientinformation/PatientInfo.js
+++ speedy-recovery-client/src/components/secured/patientinformation/PatientInfo.js
@@ -1,86 +1,13 @@
 import React, { Component } from "react";
-import { Icon, Label, Menu, Tab, Table } from "semantic-ui-react";
+import { Icon, Menu, Tab } from "semantic-ui-react";
 import "./PatientInfo.css";
 import PatientConditionPane from "./PatientConditionPane";
 import PatientCarePlanPane from "./PatientCarePlanPane";
+import PatientMedicationDispensePane from "./PatientMedicationDispensePane";
 
 class PatientInfo extends Component {
   render() {
     const { user, conditions, medicationDispenses, carePlans } = this.props;
-
-    const medicationDispensesNum = medicationDispenses.length;
-    const createMedicationDispenseTable = () => {
-      const table = [];
-      if (medicationDispensesNum) {
-        const header = [
-          <Table.Row key={"medicationDRow"}>
-            <Table.HeaderCell id="patientTableCell">#</Table.HeaderCell>
-            <Table.HeaderCell id="patientTableCell">Name</Table.HeaderCell>
-            <Table.HeaderCell id="patientTableCell" />
-            <Table.HeaderCell id="patientTableCell">Quantity</Table.HeaderCell>
-            <Table.HeaderCell id="patientTableCell">
-              Supply Time Length
-            </Table.HeaderCell>
-          </Table.Row>
-        ];
-        table.push(
-          <Table.Header id="patientTableHeader" key="medicationDTableHeader">
-            {header}
-          </Table.Header>
-        );
-      }
-      const body = [];
-      for (let i = 0; i < medicationDispensesNum; i++) {
-        const children = [];
-        const name = medicationDispenses[i].name;
-        const searchQuery = "https://www.google.com/search?q=" + name;
-
-        children.push(
-          <Table.Cell key={"medicationDOrderCell" + i}>
-            <Label ribbon>{`${i + 1}`} </Label>
-          </Table.Cell>
-        );
-        children.push(
-          <Table.Cell key={"medicationDNameCell" + i} id="patientTableCell">
-            {<h4>{name}</h4>}
-          </Table.Cell>
-        );
-        children.push(
-          <Table.Cell key={"medicationDSearchCell" + i} id="patientTableCell">
-            {
-              <h4>
-                <a href={searchQuery}>
-                  <Icon fitted name="search" />
-                </a>
-              </h4>
-            }
-          </Table.Cell>
-        );
-        children.push(
-          <Table.Cell key={"medicationDQuantityCell" + i} id="patientTableCell">
-            {<h4>{medicationDispenses[i].quantity}</h4>}
-          </Table.Cell>
-        );
-        children.push(
-          <Table.Cell key={"medicationDSupplyCell" + i} id="patientTableCell">
-            {<h4>{medicationDispenses[i].daysSupply}</h4>}
-          </Table.Cell>
-        );
-        body.push(
-          <Table.Row key={"medicationDRow2" + i} id="patientTableRow">
-            {children}
-          </Table.Row>
-        );
-      }
-      table.push(
-        <Table.Body key="medicationDTableBody" id="patientTableBody">
-          {body}
-        </Table.Body>
-      );
-      const medicationDTable = [];
-      medicationDTable.push(<Table key="medicationDTable">{table}</Table>);
-      return medicationDTable;
-    };
 
     const showGenderText = () => {
       let genderStr = "";
@@ -136,13 +63,9 @@ class PatientInfo extends Component {
           </Menu.Item>
         ),
         render: () => (
-          <Tab.Pane>
-            <h4>
-              You have {medicationDispensesNum} dispensed medications in the
-              records.
-            </h4>
-            {createMedicationDispenseTable()}
-          </Tab.Pane>
+          <PatientMedicationDispensePane
+            medicationDispenses={medicationDispenses}
+          />
         )
       },
       {
