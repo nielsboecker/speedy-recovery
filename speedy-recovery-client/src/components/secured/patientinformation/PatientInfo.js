11f3cae2076c9f9592a28b040253bab148315bc6
Ying <psyyh2@exmail.nottingham.ac.uk>
2019-03-11 02:43:19 +0000

    extract basic pane

--- speedy-recovery-client/src/components/secured/patientinformation/PatientInfo.js
+++ speedy-recovery-client/src/components/secured/patientinformation/PatientInfo.js
@@ -4,34 +4,11 @@ import "./PatientInfo.css";
 import PatientConditionPane from "./PatientConditionPane";
 import PatientCarePlanPane from "./PatientCarePlanPane";
 import PatientMedicationDispensePane from "./PatientMedicationDispensePane";
+import PatientBasicPane from "./PatientBasicPane";
 
 class PatientInfo extends Component {
   render() {
     const { user, conditions, medicationDispenses, carePlans } = this.props;
-
-    const showGenderText = () => {
-      let genderStr = "";
-      const gender = user.gender;
-      if (gender === "female") {
-        genderStr = "Girl";
-      } else if (gender === "male") {
-        genderStr = "Boy";
-      } else {
-        genderStr = gender;
-      }
-      return genderStr;
-    };
-
-    const showGenderIcon = () => {
-      let genderIcon = <Icon fitted name="smile outline" />;
-      const gender = user.gender;
-      if (gender === "female") {
-        genderIcon = <Icon color="pink" fitted name="woman" />;
-      } else if (gender === "male") {
-        genderIcon = <Icon color="blue" fitted name="man" />;
-      }
-      return genderIcon;
-    };
 
     const panes = [
       {
@@ -42,18 +19,7 @@ class PatientInfo extends Component {
           </Menu.Item>
         ),
 
-        render: () => (
-          <Tab.Pane>
-            <h4>Name: {user.name}</h4>
-            <h4>
-              Gender: {showGenderText()}
-              {showGenderIcon()}
-            </h4>
-            <h4>
-              Birthday: {user.birthDate} <Icon fitted name="birthday" />
-            </h4>
-          </Tab.Pane>
-        )
+        render: () => <PatientBasicPane user={user} />
       },
       {
         menuItem: (
