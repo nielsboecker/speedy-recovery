5b3967a5c6ecb67d59a2b5cece4aabc70872c02d
Ying <sandy19951101@126.com>
2019-03-19 03:02:37 +0000

    modify and add tests for backendMappingExtractionUtils

--- speedy-recovery-client/src/__tests__/service/BackendMappingExtractionUtils.test.js
+++ speedy-recovery-client/src/__tests__/service/BackendMappingExtractionUtils.test.js
@@ -29,61 +29,173 @@ import {
   getDate
 } from "../../service/BackendMappingExtractionUtils";
 
-test("getPosition", () => {
+const missingField = "Unknown";
+
+test("getPosition(missingResource)", () => {
   const mockResource = {};
   const mockId = "mockId";
-  getPosition(mockResource, mockId);
+  const pos = getPosition(mockResource, mockId);
+  expect(pos).toEqual("right");
 });
 
-test("getText", () => {
-  const mockResource = {};
-  getText(mockResource);
+test("getPosition(mockMessageResource)", () => {
+  const mockResource = { Recipient: "mockId" };
+  const mockId = "mockId";
+  const pos = getPosition(mockResource, mockId);
+  expect(pos).toEqual("left");
 });
 
-test("getTime", () => {
+test("getText(missingResource)", () => {
   const mockResource = {};
+  const text = getText(mockResource);
+  expect(text).toEqual(missingField);
+});
+
+test("getText(mockMessageResource)", () => {
+  const mockMessage = "mockMessage";
+  const mockResource = { Message: mockMessage };
+  const text = getText(mockResource);
+  expect(text).toEqual(mockMessage);
+});
+
+test("getTime(missingResource)", () => {
+  const mockResource = {};
+  const time = getTime(mockResource);
+  expect(time).toEqual(missingField);
+});
+
+test("getTime(mockMessageResource)", () => {
+  const mockResource = { time: "mockTime" };
   getTime(mockResource);
 });
 
-test("getUserId", () => {
+test("getUserId(missingResource,mockId)", () => {
   const mockResource = {};
   const mockId = "mockId";
-  getUserId(mockResource, mockId);
+  const id = getUserId(mockResource, mockId);
+  expect(id).toEqual(missingField);
 });
 
-test("getTitle", () => {
+test("getUserId(mockConversationResource,mockId)", () => {
+  const mockResource = { userid1: "mockId", userid2: "mockId2" };
+  const mockId = "mockId";
+  const id = getUserId(mockResource, mockId);
+  expect(id).toEqual("mockId2");
+});
+
+test("getUserId(mockConversationResource,mockId)", () => {
+  const mockResource = { userid1: "mockId1", userid2: "mockId2" };
+  const mockId = "mockId";
+  const id = getUserId(mockResource, mockId);
+  expect(id).toEqual("mockId1");
+});
+
+test("getTitle(missingResource,mockId,mockList)", () => {
   const mockResource = {};
+  const mockId = "mockId";
+  const mockList = [{}];
+  const title = getTitle(mockResource, mockId, mockList);
+  expect(title).toEqual(missingField);
+});
+
+test("getTitle(mockConversationResource,mockId,mockList)", () => {
+  const mockResource = { userid1: "mockId", userid2: "mockId2" };
   const mockId = "mockId";
   const mockList = [{}];
   getTitle(mockResource, mockId, mockList);
 });
 
-test("getId", () => {
-  const mockResource = {};
-  getId(mockResource);
+test("getTitle(mockConversationResource,mockId,mockList)", () => {
+  const mockResource = { userid1: "mockId1", userid2: "mockId2" };
+  const mockId = "mockId";
+  const mockList = [{}];
+  getTitle(mockResource, mockId, mockList);
 });
 
-test("getAvatar", () => {
+test("getTitle(mockConversationResource,mockId,mockList)", () => {
+  const mockResource = { userid1: "mockId", userid2: "mockId2" };
+  const mockId = "mockId";
+  const mockList = [{ id: "mockId2" }];
+  getTitle(mockResource, mockId, mockList);
+});
+
+test("getTitle(mockConversationResource,mockId,mockList)", () => {
+  const mockResource = { userid1: "mockId1", userid2: "mockId2" };
+  const mockId = "mockId";
+  const mockList = [{ id: "mockId1" }];
+  getTitle(mockResource, mockId, mockList);
+});
+
+test("getId(missingResource)", () => {
+  const mockResource = {};
+  const id = getId(mockResource);
+  expect(id).toEqual(missingField);
+});
+
+test("getId(mockConversationResource)", () => {
+  const cid = "mockConversationId";
+  const mockResource = { Conversation_Id: cid };
+  const id = getId(mockResource);
+  expect(id).toEqual(cid);
+});
+
+test("getAvatar(missingResource)", () => {
   const mockResource = {};
   getAvatar(mockResource);
 });
 
-test("getAlt", () => {
-  const mockResource = {};
-  getAlt(mockResource);
+test("getAvatar(mockConversationResource)", () => {
+  const mockResource = { avatar: "mockAvatar" };
+  getAvatar(mockResource);
 });
 
-test("getSubtitle", () => {
+test("getAlt(missingResource)", () => {
   const mockResource = {};
-  getSubtitle(mockResource);
+  const alt = getAlt(mockResource);
+  expect(alt).toEqual(missingField);
 });
 
-test("getUnread", () => {
-  const mockResource = {};
-  getUnread(mockResource);
+test("getAlt(mockConversationResource)", () => {
+  const mockAlt = "mockAlt";
+  const mockResource = { alt: mockAlt };
+  const alt = getAlt(mockResource);
+  expect(alt).toEqual(mockAlt);
 });
 
-test("getDate", () => {
+test("getSubtitle(missingResource)", () => {
   const mockResource = {};
+  const sub = getSubtitle(mockResource);
+  expect(sub).toEqual(missingField);
+});
+
+test("getSubtitle(mockConversationResource)", () => {
+  const mockSubtitle = "mockSubtitle";
+  const mockResource = { subtitle: mockSubtitle };
+  const sub = getSubtitle(mockResource);
+  expect(sub).toEqual(mockSubtitle);
+});
+
+test("getUnread(missingResource)", () => {
+  const mockResource = {};
+  const un = getUnread(mockResource);
+  expect(un).toEqual(missingField);
+});
+
+test("getUnread(mockConversationResource)", () => {
+  const mockUnread = "mockUnread";
+  const mockResource = { unread: mockUnread };
+  const un = getUnread(mockResource);
+  expect(un).toEqual(mockUnread);
+});
+
+test("getDate(missingResource)", () => {
+  const mockResource = {};
+  const d = getDate(mockResource);
+  expect(d).toEqual(missingField);
+});
+
+test("getDate(mockConversationResource)", () => {
+  const mockDate = "1895-11-1";
+  const mockResource = { date: mockDate };
   getDate(mockResource);
 });
